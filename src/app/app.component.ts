8150cc06f714a7c0852012100c4e37a1e001d653
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2025-03-20 20:56:12 -0600

    add game component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GameComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'lemon-brains';
